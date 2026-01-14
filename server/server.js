79a22da9858c76f5d9b6bb6a577ce5e5ad2bfe41
Deepak Kumar Yadav <yadavdeepakk9212@gmail.com>
2026-01-15 00:02:31 +0530

    last commit

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(
   cors({
     origin:[ "http://localhost:5173",
-    "https://gig-flow-murex.vercel.app/"],
+    process.env.CLIENT_URL],
     credentials: true, 
   })
 );
