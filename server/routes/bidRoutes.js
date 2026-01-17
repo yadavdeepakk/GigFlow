ce74fb6b5c06806b2eb34d14b6891af5909c3b46
Deepak Kumar Yadav <yadavdeepakk9212@gmail.com>
2026-01-17 19:25:38 +0530

    case sensetivity

--- server/routes/bidRoutes.js
+++ server/routes/bidRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import auth from "../middlewares/authMiddleware.js";
+import auth from "../middleware/authMiddleware.js";
 import {
   createBid,
   getBidsForGig,
--- server/routes/gigRoutes.js
+++ server/routes/gigRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import protect from "../middlewares/authMiddleware.js";
+import protect from "../middleware/authMiddleware.js";
 import {
   createGig,
   getMyGigs,
